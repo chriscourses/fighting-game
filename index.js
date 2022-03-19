faba446ca2d4b6b6bdb3a439f309606fe9f91e76
christopher4lis <christopher4lis@gmail.com>
2022-03-19 19:21:02 -0400

    Game Timers and Game Over

--- index.js
+++ index.js
@@ -126,6 +126,34 @@ function rectangularCollision({ rectangle1, rectangle2 }) {
   )
 }
 
+function determineWinner({ player, enemy, timerId }) {
+  clearTimeout(timerId)
+  document.querySelector('#displayText').style.display = 'flex'
+  if (player.health === enemy.health) {
+    document.querySelector('#displayText').innerHTML = 'Tie'
+  } else if (player.health > enemy.health) {
+    document.querySelector('#displayText').innerHTML = 'Player 1 Wins'
+  } else if (player.health < enemy.health) {
+    document.querySelector('#displayText').innerHTML = 'Player 2 Wins'
+  }
+}
+
+let timer = 60
+let timerId
+function decreaseTimer() {
+  if (timer > 0) {
+    timerId = setTimeout(decreaseTimer, 1000)
+    timer--
+    document.querySelector('#timer').innerHTML = timer
+  }
+
+  if (timer === 0) {
+    determineWinner({ player, enemy, timerId })
+  }
+}
+
+decreaseTimer()
+
 function animate() {
   window.requestAnimationFrame(animate)
   c.fillStyle = 'black'
@@ -173,6 +201,11 @@ function animate() {
     enemy.isAttacking = false
     player.health -= 20
     document.querySelector('#playerHealth').style.width = player.health + '%'
+  }
+
+  // end game based on health
+  if (enemy.health <= 0 || player.health <= 0) {
+    determineWinner({ player, enemy, timerId })
   }
 }
 
