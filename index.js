f8299b3f8b7755d264c15746e8d4e2c7497e6140
christopher4lis <christopher4lis@gmail.com>
2022-03-19 14:40:29 -0400

    Health Bar Interface

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ class Sprite {
     }
     this.color = color
     this.isAttacking
+    this.health = 100
   }
 
   draw() {
@@ -158,7 +159,8 @@ function animate() {
     player.isAttacking
   ) {
     player.isAttacking = false
-    console.log('go')
+    enemy.health -= 20
+    document.querySelector('#enemyHealth').style.width = enemy.health + '%'
   }
 
   if (
@@ -169,7 +171,8 @@ function animate() {
     enemy.isAttacking
   ) {
     enemy.isAttacking = false
-    console.log('enemy attack successful')
+    player.health -= 20
+    document.querySelector('#playerHealth').style.width = player.health + '%'
   }
 }
 
