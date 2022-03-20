c87a117c704b13037ad5f44b0fc11e55513abebe
christopher4lis <christopher4lis@gmail.com>
2022-03-20 14:18:21 -0400

    Player Sprite - Idle

--- index.js
+++ index.js
@@ -38,6 +38,13 @@ const player = new Fighter({
   offset: {
     x: 0,
     y: 0
+  },
+  imageSrc: './img/samuraiMack/Idle.png',
+  framesMax: 8,
+  scale: 2.5,
+  offset: {
+    x: 215,
+    y: 157
   }
 })
 
@@ -83,7 +90,7 @@ function animate() {
   background.update()
   shop.update()
   player.update()
-  enemy.update()
+  // enemy.update()
 
   player.velocity.x = 0
   enemy.velocity.x = 0
--- js/classes.js
+++ js/classes.js
@@ -1,5 +1,11 @@
 class Sprite {
-  constructor({ position, imageSrc, scale = 1, framesMax = 1 }) {
+  constructor({
+    position,
+    imageSrc,
+    scale = 1,
+    framesMax = 1,
+    offset = { x: 0, y: 0 }
+  }) {
     this.position = position
     this.width = 50
     this.height = 150
@@ -10,6 +16,7 @@ class Sprite {
     this.framesCurrent = 0
     this.framesElapsed = 0
     this.framesHold = 5
+    this.offset = offset
   }
 
   draw() {
@@ -19,15 +26,14 @@ class Sprite {
       0,
       this.image.width / this.framesMax,
       this.image.height,
-      this.position.x,
-      this.position.y,
+      this.position.x - this.offset.x,
+      this.position.y - this.offset.y,
       (this.image.width / this.framesMax) * this.scale,
       this.image.height * this.scale
     )
   }
 
-  update() {
-    this.draw()
+  animateFrames() {
     this.framesElapsed++
 
     if (this.framesElapsed % this.framesHold === 0) {
@@ -38,11 +44,31 @@ class Sprite {
       }
     }
   }
+
+  update() {
+    this.draw()
+    this.animateFrames()
+  }
 }
 
-class Fighter {
-  constructor({ position, velocity, color = 'red', offset }) {
-    this.position = position
+class Fighter extends Sprite {
+  constructor({
+    position,
+    velocity,
+    color = 'red',
+    imageSrc,
+    scale = 1,
+    framesMax = 1,
+    offset = { x: 0, y: 0 }
+  }) {
+    super({
+      position,
+      imageSrc,
+      scale,
+      framesMax,
+      offset
+    })
+
     this.velocity = velocity
     this.width = 50
     this.height = 150
@@ -59,26 +85,15 @@ class Fighter {
     this.color = color
     this.isAttacking
     this.health = 100
-  }
-
-  draw() {
-    c.fillStyle = this.color
-    c.fillRect(this.position.x, this.position.y, this.width, this.height)
-
-    // attack box
-    if (this.isAttacking) {
-      c.fillStyle = 'green'
-      c.fillRect(
-        this.attackBox.position.x,
-        this.attackBox.position.y,
-        this.attackBox.width,
-        this.attackBox.height
-      )
-    }
+    this.framesCurrent = 0
+    this.framesElapsed = 0
+    this.framesHold = 5
   }
 
   update() {
     this.draw()
+    this.animateFrames()
+
     this.attackBox.position.x = this.position.x + this.attackBox.offset.x
     this.attackBox.position.y = this.position.y
 
