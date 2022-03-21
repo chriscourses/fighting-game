f171d0e90718ed09746590a9248bb84e4bc8bcfa
christopher4lis <christopher4lis@gmail.com>
2022-03-20 20:11:57 -0400

    Player Sprite - Run

--- index.js
+++ index.js
@@ -45,6 +45,16 @@ const player = new Fighter({
   offset: {
     x: 215,
     y: 157
+  },
+  sprites: {
+    idle: {
+      imageSrc: './img/samuraiMack/Idle.png',
+      framesMax: 8
+    },
+    run: {
+      imageSrc: './img/samuraiMack/Run.png',
+      framesMax: 8
+    }
   }
 })
 
@@ -96,10 +106,13 @@ function animate() {
   enemy.velocity.x = 0
 
   // player movement
+  player.image = player.sprites.idle.image
   if (keys.a.pressed && player.lastKey === 'a') {
     player.velocity.x = -5
+    player.image = player.sprites.run.image
   } else if (keys.d.pressed && player.lastKey === 'd') {
     player.velocity.x = 5
+    player.image = player.sprites.run.image
   }
 
   // Enemy movement
--- js/classes.js
+++ js/classes.js
@@ -59,7 +59,8 @@ class Fighter extends Sprite {
     imageSrc,
     scale = 1,
     framesMax = 1,
-    offset = { x: 0, y: 0 }
+    offset = { x: 0, y: 0 },
+    sprites
   }) {
     super({
       position,
@@ -88,6 +89,12 @@ class Fighter extends Sprite {
     this.framesCurrent = 0
     this.framesElapsed = 0
     this.framesHold = 5
+    this.sprites = sprites
+
+    for (const sprite in this.sprites) {
+      sprites[sprite].image = new Image()
+      sprites[sprite].image.src = sprites[sprite].imageSrc
+    }
   }
 
   update() {
