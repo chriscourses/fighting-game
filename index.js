f1149544734c1f272b8ad88439af865a4dd17a0a
christopher4lis <christopher4lis@gmail.com>
2022-03-23 13:08:52 -0400

    Interface Design and Animation

--- index.js
+++ index.js
@@ -172,6 +172,8 @@ function animate() {
   c.fillRect(0, 0, canvas.width, canvas.height)
   background.update()
   shop.update()
+  c.fillStyle = 'rgba(255, 255, 255, 0.15)'
+  c.fillRect(0, 0, canvas.width, canvas.height)
   player.update()
   enemy.update()
 
@@ -227,7 +229,9 @@ function animate() {
     enemy.takeHit()
     player.isAttacking = false
 
-    document.querySelector('#enemyHealth').style.width = enemy.health + '%'
+    gsap.to('#enemyHealth', {
+      width: enemy.health + '%'
+    })
   }
 
   // if player misses
@@ -246,7 +250,10 @@ function animate() {
   ) {
     player.takeHit()
     enemy.isAttacking = false
-    document.querySelector('#playerHealth').style.width = player.health + '%'
+
+    gsap.to('#playerHealth', {
+      width: player.health + '%'
+    })
   }
 
   // if player misses
